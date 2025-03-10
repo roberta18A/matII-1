14813418813483019bc7bf8054cb0723d5c33410
roberta18A <roberta.rodrigues18@escola.pr.gov.br>
2025-03-10 11:57:57 -0300

    aula 4

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.addEventlistener('DOMContetLoaded', function()){
     const aumentaFonteBtao= document.getElementById('aumentar-fonte');
 
-
+   let tamanhoAtualfonte= 1;
 }
+   aumentaFonteBtao.addEventlistener('click', function ) {
+   tamanhoAtualfonte 
+   }) 
